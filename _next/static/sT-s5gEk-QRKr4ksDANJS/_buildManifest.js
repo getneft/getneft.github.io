8f83ab3c0341af12572e900950998dd463281811
Andrey Gulitsky <angulitsky@gmail.com>
2024-08-05 15:44:27 +0600

    Add files via upload

---
+++ _next/static/sT-s5gEk-QRKr4ksDANJS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a7bac88e914bc0c2.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
